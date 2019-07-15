bebd47b13665d97eb2ba83fa9fe93eace7f2aa43
huangnuozhong <171628543@qq.com>
2019-07-15 17:25:28 +0800

    add mybatis_day02

---
+++ mybatis_day02/src/main/java/com/mybatis/dao/IUserDao.java
@@ -0,0 +1,67 @@
+package com.mybatis.dao;
+
+import com.mybatis.domain.QueryVo;
+import com.mybatis.domain.User;
+
+import java.util.List;
+
+public interface IUserDao {
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 保存用户
+     *
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     *
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据Id删除用户
+     *
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户信息
+     *
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     *
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 查询总用户数
+     *
+     * @return
+     */
+    int findTotal();
+
+    /**
+     * 根据queryVo中的条件查询用户
+     *
+     * @param vo
+     * @return
+     */
+    List<User> findUserByVo(QueryVo vo);
+}
