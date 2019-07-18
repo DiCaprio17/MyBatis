49c35d30d66daa86f979e5a081a723a074851512
huangnuozhong <171628543@qq.com>
2019-07-18 15:45:21 +0800

    mybatis_day03

---
+++ mybatis_day03/day03_eesy_03one2many/src/main/java/com/itheima/dao/IAccountDao.java
@@ -0,0 +1,21 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Account;
+import com.itheima.domain.AccountUser;
+
+import java.util.List;
+
+public interface IAccountDao {
+
+    /**
+     * 查询所有账户，同时还要获取到当前账户的所属用户信息
+     * @return
+     */
+    List<Account> findAll();
+
+    /**
+     * 查询所有账户，并且带有用户名称和地址信息
+     * @return
+     */
+    List<AccountUser> findAllAccount();
+}
---
+++ mybatis_day03/day03_eesy_04many2many/src/main/java/com/itheima/dao/IRoleDao.java
@@ -0,0 +1,14 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Role;
+
+import java.util.List;
+
+public interface IRoleDao {
+
+    /**
+     * 查询所有角色
+     * @return
+     */
+    List<Role> findAll();
+}
